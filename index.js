fd064a4eaae8338f3ccc1a46f1039640c6f8eafa
rahul-daffodil <rahul.kamboj@daffodilsw.com>
2015-12-08 12:55:50 +0530

    starting of project

---
+++ index.js
@@ -0,0 +1,22 @@
+/*
+ *  node debug index.js
+ *  cont to go to next checkpoint
+ *  repl to evaluate the context at any point
+ *  next to reach next statement
+ *
+ * */
+
+
+
+// Example code for debugging
+var x= 5;
+setTimeout(function(){
+    debugger;
+    console.log('World !!!')
+
+
+},1000)
+console.log('Hello')
+
+
+
